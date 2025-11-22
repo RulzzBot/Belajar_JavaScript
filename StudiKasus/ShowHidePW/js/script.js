41ecbdc17d4e7601195b18e470a406e59881afa3
YutaDev <nazrulwijaya0@gmail.com>
2025-11-22 10:30:40 +0700

    update fitur terbaru

--- StudiKasus/ShowHidePW/js/script.js
+++ StudiKasus/ShowHidePW/js/script.js
@@ -1,19 +1,47 @@
 const show = document.getElementById("show")
 const hide = document.getElementById("hide")
-const input = document.getElementById("password")
+const ShowHide = document.querySelector(".test") 
+const inputPW = document.getElementById("password")  ;
+const inputUser = document.getElementById("Email");
+const submit = document.getElementById("btn")
+const rmbr = document.getElementById("rmbr");
 
 show.addEventListener("click", ()=>{
     console.log("Haloo")
     show.classList.add("hidden");
     hide.classList.remove("hidden")
-    input.setAttribute("type", "text")
+    ShowHide.setAttribute("type", "text")
 })
 
 hide.addEventListener("click", ()=>{
     console.log("Haloo")
     show.classList.remove("hidden");
     hide.classList.add("hidden")
-    input.setAttribute("type", "password")
+    ShowHide.setAttribute("type", "password")
 })
+
+submit.addEventListener("click", ()=> {
+
+    const userVal = inputUser.value.trim();
+    const userPW = inputPW.value.trim();
+
+    if(userVal == "" && userPW == "" ){
+        swal.fire({
+            title: "Input User & PW",
+            text:"Input Tidak Boleh Kosong",
+            icon: "info"
+        });
+    }else{
+        console.log("Log Berhasil")
+    }
+
+    console.log(`Username : ${userVal}`)
+    console.log(`Password : ${userPW}`)
+
+    inputUser.value = ""
+    inputPW.value = ""
+
+})
+
 
 
